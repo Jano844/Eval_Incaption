b368213808793db1afa5381d6342f656806376d7
Jan Sanger <jsanger@2-D-2.42heilbronn.de>
2024-09-26 17:48:11 +0200

    leer

--- srcs/requirements/nginx/tools/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# (its the entrypoint.sh)
-
-# can create the ssl_certificate with open ssl here, but for simplification
-# i just createt them only once and have them in the conf/ssl directory
-
-# start the main procces
-exec nginx -g 'daemon off;' 
